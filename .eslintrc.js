513eb164b95cff4fa2ed95006d94abeb65249b53
Kyle King <kmking72@gmail.com>
2017-08-12 12:10:23 -0400

    Update SDK  &  Add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "globals": {
+        "Pebble": true,
+        "localStorage": true,
+        "XMLHttpRequest": true,
+        "define": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "off"
+    }
+};
